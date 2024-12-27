0f13f43c0438a2ec860bf06997fac4fda24f930f
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2024-12-27 21:13:09 +0000

    Moves lambda to package

--- infrastructure/lib/temp_cdk_stack-stack.ts
+++ infrastructure/lib/temp_cdk_stack-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "aws-cdk-lib"
+import * as path from "path"
 import { Construct } from "constructs"
 import * as Lambda from "aws-cdk-lib/aws-lambda"
 import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs"
@@ -9,14 +10,20 @@ export class TempCdkStackStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props)
 
+		const projectRoot = "../"
+		const lambdaDirectory = path.join(projectRoot, "packages/lambdas")
+
 		// a policy that allows the lambda to access the translate service
 		const translateAccessPolicy = new iam.PolicyStatement({
 			actions: ["translate:TranslateText"],
 			resources: ["*"],
 		})
 
+		const translateLambdaPath = path.resolve(
+			path.join(lambdaDirectory, "translate/index.ts")
+		)
 		const lambdaFunc = new lambdaNodejs.NodejsFunction(this, "translate", {
-			entry: "lambda/translate.ts",
+			entry: translateLambdaPath,
 			handler: "index",
 			runtime: Lambda.Runtime.NODEJS_20_X,
 			initialPolicy: [translateAccessPolicy],
